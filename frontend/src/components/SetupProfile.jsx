ace4429dff565d5e5045f9dc8b1c6ac2a7050a35
Allen Baek <allenbaek@Allens-MacBook-Pro.local>
2021-03-05 17:48:29 -0800

    SetUp Profile and styles

---
+++ frontend/src/components/SetupProfile.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
+import profile from "./profilePics/background.jpg";
+import "./Styles.css";
+import { Link } from "react-router-dom";
+
+export default function SetupProfile() {
+  const [bios, setBios] = useState("");
+  const [achieve, setAchieve] = useState("");
+
+  // updates on input
+  const handleChangeB = ({ target }) => {
+    setBios(target.value);
+  };
+
+  const handleChangeA = ({ target }) => {
+    setAchieve(target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log("Bios : " + bios);
+    console.log("Achievements : " + achieve);
+  };
+
+  
+  return (
+    <div>
+      {/* Background Box*/}
+      <div className="box"></div>
+
+      {/* ProfileImage */}
+      <div className="container">
+        <img src={profile} alt="Image_file" id="clip" />
+      </div>
+
+      <div className="goBack_placement">
+      <Link to="./">
+        <Button>Profile Page</Button>
+        </Link>
+      </div>
+
+      {/* Input fields */}
+      <div className="inputs">
+        <Form>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <Form.Group as={Row} controlId="Bios">
+            <Form.Label column sm="2">
+              Biography
+            </Form.Label>
+
+            <Col sm="8">
+              <Form.Control
+                type="text"
+                as="textarea"
+                placeholder="About Yourself"
+                value={bios}
+                onChange={handleChangeB}
+                rows={3}
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} controlId="Achievement">
+            <Form.Label column sm="2">
+              Achievements
+            </Form.Label>
+
+            <Col sm="8">
+              <Form.Control
+                type="text"
+                as="textarea"
+                placeholder="Your Achievements"
+                value={achieve}
+                onChange={handleChangeA}
+                rows={3}
+              />
+            </Col>
+          </Form.Group>
+
+          <Button onClick={handleSubmit}> Save </Button>
+        </Form>
+      </div>
+    </div>
+  );
+}
