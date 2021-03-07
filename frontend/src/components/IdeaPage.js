606b807d526edd8d59b65b46800d383e7469e4db
rheadash <rhea.dash@gmail.com>
2021-03-07 13:34:09 -0800

    Everything except voting & nav bar.

--- frontend/src/components/IdeaPage.js
+++ frontend/src/components/IdeaPage.js
@@ -1,12 +1,30 @@
 import React from 'react'
-import { Card, Row, Col, Container } from 'react-bootstrap'
+import UpArrow from './uparrow.png'
+import DownArrow from './downarrow.png'
+import { Card, Row, Col, Container, Button, Table } from 'react-bootstrap'
+
+const styles = {
+  circle: {
+    backgroundColor: 'black',
+    borderRadius: '50%',
+    height: 100,
+    width: 100,
+  },
+  buttonRight: {
+    float: 'right',
+  },
+  votingButton: {
+    float: 'left',
+    padding: 10,
+  },
+}
 
 const IdeaPage = () => {
   return (
     <Container>
       {/* Top row */}
-      <Row fluid>
-        <Col xs={8}>
+      <Row>
+        <Col md={8}>
           <Card>
             {/* Will replace with actual title later */}
             <Card.Header as='h2'>Title of idea</Card.Header>
@@ -25,28 +43,103 @@ const IdeaPage = () => {
             </Card.Body>
           </Card>
         </Col>
-        <Col>
+        <Col md={2}>
           <Card>
             <Card.Header> Popularity</Card.Header>
+            <Card.Body>
+              <Table striped borderless hover>
+                <tr>
+                  <td>
+                    <Button
+                      img
+                      src={UpArrow}
+                      style={styles.votingButton}
+                      size='sm'></Button>
+                  </td>
+                  <td>+++</td>
+                </tr>
+                <tr>
+                  <td>
+                    <Button
+                      img
+                      src={DownArrow}
+                      style={styles.votingButton}
+                      size='sm'></Button>
+                  </td>
+                  <td>---</td>
+                </tr>
+              </Table>
+              Images
+            </Card.Body>
           </Card>
         </Col>
-        <Col>
+        <Col md={2}>
+          <div style={styles.circle}></div>
           <Card>
             <Card.Header> Bio</Card.Header>
+            <Card.Body>Text</Card.Body>
           </Card>
         </Col>
       </Row>
       <br />
       {/* Bottom row */}
       <Row>
-        <Col>
+        <Col md={6}>
           <Card>
-            <Card.Header> Comments</Card.Header>
+            <Card.Header>
+              {' '}
+              Comments
+              <Button style={styles.buttonRight} size='sm'>
+                Add new comment
+              </Button>
+            </Card.Header>
+            <Card.Body>
+              <Table bordered hover>
+                <tbody>
+                  <tr>
+                    <td style={{ padding: 20 }}>new comment</td>
+                  </tr>
+                  <tr>
+                    <td style={{ padding: 20 }}>new comment</td>
+                  </tr>
+                  <tr>
+                    <td style={{ padding: 20 }}>new comment</td>
+                  </tr>
+                  <tr>
+                    <td style={{ padding: 20 }}>new comment</td>
+                  </tr>{' '}
+                </tbody>{' '}
+              </Table>
+            </Card.Body>
           </Card>
         </Col>
-        <Col>
+        <Col md={6}>
           <Card>
-            <Card.Header> Criticism</Card.Header>
+            <Card.Header>
+              {' '}
+              Criticisim
+              <Button style={styles.buttonRight} size='sm'>
+                Add new criticisim
+              </Button>
+            </Card.Header>
+            <Card.Body>
+              <Table bordered hover>
+                <tbody>
+                  <tr>
+                    <td style={{ padding: 20 }}>new criticisim</td>
+                  </tr>
+                  <tr>
+                    <td style={{ padding: 20 }}>new criticisim</td>
+                  </tr>
+                  <tr>
+                    <td style={{ padding: 20 }}>new criticisim</td>
+                  </tr>
+                  <tr>
+                    <td style={{ padding: 20 }}>new criticisim</td>
+                  </tr>{' '}
+                </tbody>
+              </Table>
+            </Card.Body>
           </Card>
         </Col>
       </Row>
