fa8d9793eeaf75035e897d16f222ea81ce3864be
Allen Baek <allenbaek@Allens-MacBook-Pro.local>
2021-03-08 17:39:09 -0800

    small changes

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -19,14 +19,6 @@ export default function Profile() {
 
   return (
     <div className='scrolling'>
-      {/* This is needed to get the Card working */}
-      <link
-        rel='stylesheet'
-        href='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
-        integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T'
-        crossorigin='anonymous'
-      />
-
       {/* Background Box*/}
       <div className='box'></div>
 
