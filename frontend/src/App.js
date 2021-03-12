a2c2abff390b35dfe1d8e7c344d8aaa05255f124
ahrorus <ab.ahror@gmail.com>
2021-03-12 14:27:12 -0800

    Create home branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import Home from './components/Home'
+import MyNavbar from './components/MyNavbar'
 
 const App = () => {
-  return <div>Hello world</div>
+  return (
+    <div>
+      <MyNavbar />
+      <div className='d-flex justify-content-between pt-5 mt-5 pl-5 ml-5 pr-4'>
+        <div class='pr-4'>
+          <Home className='home' />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
