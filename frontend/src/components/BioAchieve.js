5b92076bd247db0d2d4145b616e60672f5aa7ead
Allen Baek <baekallen@gmail.com>
2021-04-03 17:09:37 -0700

    Separate Bios/Achieve page and made a new Page BioAchieve.js

---
+++ frontend/src/components/BioAchieve.js
@@ -0,0 +1,172 @@
+import React, { useState } from 'react'
+import { Button, Form, Row, Col } from 'react-bootstrap'
+import profileImage from '../images/background.png'
+
+const BioAchieve = () => {
+  const [bios, setBios] = useState('My bios')
+  const [achieve, setAchieve] = useState('My Achievements')
+  const [edit, setEdit] = useState(false)
+
+  // updates on input
+  const handleChangeB = ({ target }) => {
+    setBios(target.value)
+  }
+
+  const handleChangeA = ({ target }) => {
+    setAchieve(target.value)
+  }
+
+  // Saves Input to database
+  const handleSubmit = () => {
+    console.log('Bios : ' + bios)
+    console.log('Achievements : ' + achieve)
+
+    // use axios to send values to the server
+  }
+
+  //Turns on editor
+  const switchstateT = () => {
+    setEdit(true)
+  }
+  //Turns off editor
+  const switchstateF = () => {
+    setEdit(false)
+  }
+  //Resets values when user cancels changes
+  const resetValues = () => {
+    // use axios to get values from the server
+  }
+
+  /* for EditProfile */
+  const inputs = {
+    position: 'absolute',
+    top: '60%',
+    left: '50%',
+    width: '800px',
+
+    transform: 'translate(-50%, -50%)',
+  }
+
+  /* Location ProfilePic  */
+  const container = {
+    position: 'fixed',
+    top: '5.5%',
+    left: '40%',
+  }
+  // edits profile picture
+  const clip = {
+    position: 'absolute',
+    clipPath: 'circle(40%)',
+  }
+  // Button to go back to Profile Page
+  const EditPlacement = {
+    position: 'absolute',
+
+    top: '20%',
+    left: '70%',
+  }
+
+  //For Bios and Achievement access
+  var input1 = <Form.Control />
+  var input2 = <Form.Control />
+  var button = <Button></Button>
+  var submit = ''
+
+  if (edit) {
+    input1 = (
+      <Form.Control
+        type='text'
+        as='textarea'
+        placeholder='About Yourself'
+        value={bios}
+        onChange={handleChangeB}
+        rows={3}
+      />
+    )
+
+    input2 = (
+      <Form.Control
+        type='text'
+        as='textarea'
+        placeholder='Your Achievements'
+        value={achieve}
+        onChange={handleChangeA}
+        rows={3}
+      />
+    )
+
+    button = <Button onClick={(resetValues, switchstateF)}>Cancel</Button>
+
+    submit = <Button onClick={(handleSubmit, switchstateF)}> Save </Button>
+  } else {
+    input1 = (
+      <Form.Control
+        type='text'
+        as='textarea'
+        placeholder='About Yourself'
+        value={bios}
+        onChange={handleChangeB}
+        rows={3}
+        disabled
+      />
+    )
+
+    input2 = (
+      <Form.Control
+        type='text'
+        as='textarea'
+        placeholder='Your Achievements'
+        value={achieve}
+        onChange={handleChangeA}
+        rows={3}
+        disabled
+      />
+    )
+
+    button = <Button onClick={switchstateT}>Edit</Button>
+
+    submit = ''
+  }
+
+  //   placeholder to print out project ideas etc.
+  const displayBioAchieve = (
+    <div>
+      {/* ProfileImage */}
+      <div style={container}>
+        <img src={profileImage} alt='Image_file' style={clip} />
+      </div>
+      {/* Go to Profile page button */}
+      <div style={EditPlacement}>
+        <Button>Profile</Button>
+      </div>
+      Input fields
+      <div style={inputs}>
+        <Form>
+          <Form.Group as={Row} controlId='Bios'>
+            <Form.Label column sm='2'>
+              Biography
+            </Form.Label>
+
+            <Col sm='8'>{input1}</Col>
+          </Form.Group>
+
+          <Form.Group as={Row} controlId='Achievement'>
+            <Form.Label column sm='2'>
+              Achievements
+            </Form.Label>
+
+            <Col sm='8'>{input2}</Col>
+          </Form.Group>
+          {button}
+          <br />
+          <br />
+          {submit}
+          <br />
+        </Form>
+      </div>
+    </div>
+  )
+  return <div>{displayBioAchieve}</div>
+}
+
+export default BioAchieve
--- frontend/src/components/ProfileEditor.js
+++ frontend/src/components/ProfileEditor.js
@@ -1,77 +1,11 @@
 import React, { useState } from 'react'
 import { Button, Form, Row, Col } from 'react-bootstrap'
 import profileImage from '../images/background.png'
+import BioAchieve from './BioAchieve'
 import MyNavbar from './MyNavbar'
 
 const ProfileEditor = () => {
-  const [bios, setBios] = useState('My bios')
-  const [achieve, setAchieve] = useState('My Achievements')
-  const [edit, setEdit] = useState(false)
-
-  // updates on input
-  const handleChangeB = ({ target }) => {
-    setBios(target.value)
-  }
-
-  const handleChangeA = ({ target }) => {
-    setAchieve(target.value)
-  }
-
-  // Saves Input to database
-  const handleSubmit = () => {
-    console.log('Bios : ' + bios)
-    console.log('Achievements : ' + achieve)
-
-    // axios
-  }
-
-  const switchstateT = () => {
-    setEdit(true)
-  }
-
-  const switchstateF = () => {
-    setEdit(false)
-  }
-
-  //Style
-
-  const box = {
-    float: 'left',
-    height: '80px',
-    width: '100%',
-    marginBottom: '15px',
-    clear: 'both',
-
-    backgroundColor: '#2b7a98',
-  }
-  /* for EditProfile */
-  const inputs = {
-    position: 'absolute',
-    top: '60%',
-    left: '50%',
-    width: '800px',
-
-    transform: 'translate(-50%, -50%)',
-  }
-
-  /* Location ProfilePic  */
-  const container = {
-    position: 'fixed',
-    top: '5.5%',
-    left: '40%',
-  }
-  // edits profile picture
-  const clip = {
-    position: 'absolute',
-    clipPath: 'circle(40%)',
-  }
-  // Button to go back to Profile Page
-  const EditPlacement = {
-    position: 'absolute',
-
-    top: '20%',
-    left: '70%',
-  }
+  const [page, setPage] = useState('BioAch')
 
   const scrolling = {
     paddingBottom: 'auto',
@@ -80,67 +14,11 @@ const ProfileEditor = () => {
     maxWidth: '100%',
     overflowX: 'hidden',
   }
+  var thePage = 'hello'
 
-  //For Bios and Achievement editibility
-  var input1 = <Form.Control />
-  var input2 = <Form.Control />
-  var button = <Button></Button>
-  var submit = ''
-
-  if (edit) {
-    input1 = (
-      <Form.Control
-        type='text'
-        as='textarea'
-        placeholder='About Yourself'
-        value={bios}
-        onChange={handleChangeB}
-        rows={3}
-      />
-    )
-
-    input2 = (
-      <Form.Control
-        type='text'
-        as='textarea'
-        placeholder='Your Achievements'
-        value={achieve}
-        onChange={handleChangeA}
-        rows={3}
-      />
-    )
-
-    button = <Button onClick={switchstateF}>Cancel</Button>
-
-    submit = <Button onClick={(handleSubmit, switchstateF)}> Save </Button>
-  } else {
-    input1 = (
-      <Form.Control
-        type='text'
-        as='textarea'
-        placeholder='About Yourself'
-        value={bios}
-        onChange={handleChangeB}
-        rows={3}
-        disabled
-      />
-    )
-
-    input2 = (
-      <Form.Control
-        type='text'
-        as='textarea'
-        placeholder='Your Achievements'
-        value={achieve}
-        onChange={handleChangeA}
-        rows={3}
-        disabled
-      />
-    )
-
-    button = <Button onClick={switchstateT}>Edit</Button>
-
-    submit = ''
+  //Base Page edit Bio and Achievement
+  if (page.localeCompare('BioArch')) {
+    thePage = 'hello World'
   }
 
   return (
@@ -148,40 +26,7 @@ const ProfileEditor = () => {
       {/* Navbar*/}
       <MyNavbar />
 
-      {/* ProfileImage */}
-      <div style={container}>
-        <img src={profileImage} alt='Image_file' style={clip} />
-      </div>
-
-      {/* Go to Profile page button */}
-      <div style={EditPlacement}>
-        <Button>Profile</Button>
-      </div>
-
-      {/* Input fields */}
-      <div style={inputs}>
-        <Form>
-          <Form.Group as={Row} controlId='Bios'>
-            <Form.Label column sm='2'>
-              Biography
-            </Form.Label>
-
-            <Col sm='8'>{input1}</Col>
-          </Form.Group>
-
-          <Form.Group as={Row} controlId='Achievement'>
-            <Form.Label column sm='2'>
-              Achievements
-            </Form.Label>
-
-            <Col sm='8'>{input2}</Col>
-          </Form.Group>
-          {button}
-          <br />
-          <br />
-          {submit}
-        </Form>
-      </div>
+      <BioAchieve />
     </div>
   )
 }
