ae594aaf355b386bc058a5685b6bad9790f96c1e
Thang Cao <shiyoline@gmail.com>
2021-05-04 14:53:57 -0700

    Fix voting issues

--- frontend/src/components/Idea.js
+++ frontend/src/components/Idea.js
@@ -9,156 +9,157 @@ import axios from 'axios'
 import Notification from './Notification'
 
 const Idea = ({ idea }) => {
-	const [vote, setVote] = useState(idea.upVote - idea.downVote)
-	const [errorMessage, setErrorMessage] = useState(null)
+  let vote = idea.upVote - idea.downVote
+  const [errorMessage, setErrorMessage] = useState(null)
 
-	/* initially idea vote state is neutral */
-	const [neutral, setNeutral] = useState(true)
-	const [upVoted, setUpVoted] = useState(false)
-	const [downVoted, setDownVoted] = useState(false)
+  /* initially idea vote state is neutral */
+  const [neutral, setNeutral] = useState(true)
+  const [upVoted, setUpVoted] = useState(false)
+  const [downVoted, setDownVoted] = useState(false)
 
-	const upVoteStyle = {
-		width: '30px',
-		height: '30px',
-	}
+  const upVoteStyle = {
+    width: '30px',
+    height: '30px',
+  }
 
-	const downVoteStyle = {
-		width: '30px',
-		height: '30px',
-	}
+  const downVoteStyle = {
+    width: '30px',
+    height: '30px',
+  }
 
-	const authorStyle = {
-		color: '#2b7a98',
-	}
+  const authorStyle = {
+    color: '#2b7a98',
+  }
 
-	const handleUpVote = async () => {
-		const loggedUserJSON = window.localStorage.getItem('loggedInUser')
+  const handleUpVote = async () => {
+    const loggedUserJSON = window.localStorage.getItem('loggedInUser')
 
-		if (!loggedUserJSON) {
-			setErrorMessage('Oh no! You must be logged in to vote on ideas.')
-			setTimeout(() => {
-				setErrorMessage(null)
-			}, 5000)
-			return
-		}
+    if (!loggedUserJSON) {
+      setErrorMessage('Oh no! You must be logged in to vote on ideas.')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+      return
+    }
 
-		if (!neutral && upVoted) {
-			setErrorMessage('Error! You already up-voted this idea.')
-			setTimeout(() => {
-				setErrorMessage(null)
-			}, 5000)
+    if (!neutral && upVoted) {
+      setErrorMessage('Error! You already up-voted this idea.')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
 
-			return
-		}
+      return
+    }
 
-		try {
-			await axios.post(`/api/ideas/${idea.id}/rating`, { type: 'upVote' })
+    try {
+      await axios.post(`/api/ideas/${idea.id}/rating`, { type: 'upVote' })
+      if (downVoted) {
+        setDownVoted(false)
+        setNeutral(true)
+      } else {
+        setUpVoted(true)
+        setNeutral(false)
+      }
+    } catch (error) {
+      console.log('upvote error')
+    }
+  }
 
-			if (downVoted) {
-				setDownVoted(false)
-				setNeutral(true)
-			} else {
-				setUpVoted(true)
-				setNeutral(false)
-			}
+  const handleDownVote = async () => {
+    const loggedUserJSON = window.localStorage.getItem('loggedInUser')
 
-			setVote(vote + 1)
-		} catch (error) {
-			console.log('upvote error')
-		}
-	}
+    if (!loggedUserJSON) {
+      setErrorMessage('Oh no! You must be logged in to vote on ideas.')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+      return
+    }
+    if (!neutral && downVoted) {
+      setErrorMessage('Error! You already down-voted this idea.')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
 
-	const handleDownVote = async () => {
-		const loggedUserJSON = window.localStorage.getItem('loggedInUser')
+      return
+    }
 
-		if (!loggedUserJSON) {
-			setErrorMessage('Oh no! You must be logged in to vote on ideas.')
-			setTimeout(() => {
-				setErrorMessage(null)
-			}, 5000)
-			return
-		}
-		if (!neutral && downVoted) {
-			setErrorMessage('Error! You already down-voted this idea.')
-			setTimeout(() => {
-				setErrorMessage(null)
-			}, 5000)
+    try {
+      await axios.post(`/api/ideas/${idea.id}/rating`, { type: 'downVote' })
+      if (upVoted) {
+        setUpVoted(false)
+        setNeutral(true)
+      } else {
+        setDownVoted(true)
+        setNeutral(false)
+      }
+    } catch (error) {
+      console.log('down vote error')
+    }
+  }
 
-			return
-		}
+  const displayVote = () => {
+    if (upVoted) vote++
+    if (downVoted) vote--
 
-		try {
-			await axios.post(`/api/ideas/${idea.id}/rating`, { type: 'downVote' })
+    return vote
+  }
 
-			if (upVoted) {
-				setUpVoted(false)
-				setNeutral(true)
-			} else {
-				setDownVoted(true)
-				setNeutral(false)
-			}
+  return (
+    <div className='mt-4 mb-4 d-flex justify-content-between'>
+      <div className='mt-1 mr-1'>
+        {/* =============UPVOTE/DOWNVOTE============ */}
+        <ButtonGroup vertical>
+          {/* === upvote === */}
+          <Button variant='link' onClick={handleUpVote}>
+            <Image
+              style={upVoteStyle}
+              src={upVoted ? upvoteActiveImage : upvoteImage}></Image>
+          </Button>
 
-			setVote(vote - 1)
-		} catch (error) {
-			console.log('down vote error')
-		}
-	}
+          {/* === vote display === */}
+          <div className='col text-center'>
+            <h5>{displayVote()}</h5>
+          </div>
 
-	return (
-		<div className='mt-4 mb-4 d-flex justify-content-between'>
-			<div className='mt-1 mr-1'>
-				{/* =============UPVOTE/DOWNVOTE============ */}
-				<ButtonGroup vertical>
-					{/* === upvote === */}
-					<Button variant='link' onClick={handleUpVote}>
-						<Image
-							style={upVoteStyle}
-							src={upVoted ? upvoteActiveImage : upvoteImage}></Image>
-					</Button>
+          {/* === downvote === */}
+          <Button variant='link' onClick={handleDownVote}>
+            <Image
+              style={downVoteStyle}
+              src={downVoted ? downvoteActiveImage : downvoteImage}></Image>
+          </Button>
+        </ButtonGroup>
+      </div>
 
-					{/* === vote display === */}
-					<div className='col text-center'>
-						<h5>{vote}</h5>
-					</div>
-
-					{/* === downvote === */}
-					<Button variant='link' onClick={handleDownVote}>
-						<Image
-							style={downVoteStyle}
-							src={downVoted ? downvoteActiveImage : downvoteImage}></Image>
-					</Button>
-				</ButtonGroup>
-			</div>
-
-			<Card border='info' style={{ width: '100%' }}>
-				<Notification message={errorMessage} />
-				{/* =============TITLE AND DATE============ */}
-				<Card.Header>
-					<div className='d-flex align-items-center justify-content-between'>
-						<h5>{idea.title}</h5>
-						<p>Created on: {idea.date.substring(0, 10)}</p>
-					</div>
-				</Card.Header>
-				{/* =============DESCRIPTION AND AUTHOR============ */}
-				<Card.Body>
-					<Card.Text>{idea.problemStatement.domain}</Card.Text>
-					<div className='d-flex justify-content-between'>
-						<p>
-							<Link to={'/profile/' + idea.user} style={authorStyle}>
-								<b>Author</b>: {idea.author}
-							</Link>
-						</p>
-						<p>
-							<b>Category:</b> {idea.category}
-						</p>
-					</div>
-					<Link to={'/ideapage/' + idea.id}>
-						<Button variant='info'>Go to the idea page</Button>
-					</Link>
-				</Card.Body>
-			</Card>
-		</div>
-	)
+      <Card border='info' style={{ width: '100%' }}>
+        <Notification message={errorMessage} />
+        {/* =============TITLE AND DATE============ */}
+        <Card.Header>
+          <div className='d-flex align-items-center justify-content-between'>
+            <h5>{idea.title}</h5>
+            <p>Created on: {idea.date.substring(0, 10)}</p>
+          </div>
+        </Card.Header>
+        {/* =============DESCRIPTION AND AUTHOR============ */}
+        <Card.Body>
+          <Card.Text>{idea.problemStatement.domain}</Card.Text>
+          <div className='d-flex justify-content-between'>
+            <p>
+              <Link to={'/profile/' + idea.user} style={authorStyle}>
+                <b>Author</b>: {idea.author}
+              </Link>
+            </p>
+            <p>
+              <b>Category:</b> {idea.category}
+            </p>
+          </div>
+          <Link to={'/ideapage/' + idea.id}>
+            <Button variant='info'>Go to the idea page</Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    </div>
+  )
 }
 
 export default Idea
